4300d164ef082b0d3743a141b53cd8f5b9a75d5f
a690089735 <690089735@qq.com>
2023-02-01 13:25:07 +0800

    t

--- _sync/Python/playwrightDemo.py
+++ _sync/Python/playwrightDemo.py
@@ -3,11 +3,11 @@ from playwright.async_api import async_playwright
 
 async def main():
     async with async_playwright() as p:
-        for browser_type in [p.chromium, p.firefox, p.webkit]:
+        for browser_type in [p.chromium, p.firefox, p.webkit]: ## 这里很厉害啊,直接调用多种浏览器
             browser = await browser_type.launch()
             page = await browser.new_page()
-            await page.goto('http://whatsmyuseragent.org/')
-            await page.screenshot(path=f'example-{browser_type.name}.png')
+            await page.goto('https://bot.sannysoft.com/')
+            await page.screenshot(path=f'example-{browser_type.name}.png') ## p.firefox伪装性最好,不会被识别为webDriver
             await browser.close()
 
 asyncio.run(main())
--- _sync/Python/zei8_get_nlist.py
+++ _sync/Python/zei8_get_nlist.py
@@ -1,31 +1,34 @@
 import asyncio
+import os
+import re
 from pyppeteer import launch
-## 以后用playwright-python
-
+# 以后用playwright-python
 classList = [
-    "https://www.zei8.vip/xuanhuan",
-    "https://www.zei8.vip/chuantongwuxia",
-    "https://www.zei8.vip/kongbujing",
-    "https://www.zei8.vip/xiandaidushi",
-    "https://www.zei8.vip/renwuchuanji",
-    "https://www.zei8.vip/wangyoudongman",
-    "https://www.zei8.vip/junshilishi",
-    "https://www.zei8.vip/redianziyuan",
-    "https://www.zei8.vip/xifangqihuan",
-    "https://www.zei8.vip/xianxiaxiuzhen",
-    "https://www.zei8.vip/zhentantuili",
-    "https://www.zei8.vip/jingdiankehuan",
-    "https://www.zei8.vip/mingzhuzazhi",
-    "https://www.zei8.vip/lishichuanyue",
-    "https://www.zei8.vip/chuantongyanqing",
-    "https://www.zei8.vip/dushishehui",
-    "https://www.zei8.vip/huanxiangxianyan",
-    "https://www.zei8.vip/danmeibaihe"
+    ['东方玄幻','https://www.zei8.vip/xuanhuan'],
+    ['传统武侠','https://www.zei8.vip/chuantongwuxia'],
+    ['恐怖惊悚','https://www.zei8.vip/kongbujing'],
+    ['现代都市','https://www.zei8.vip/xiandaidushi'],
+    ['人物传记','https://www.zei8.vip/renwuchuanji'],
+    ['网游动漫','https://www.zei8.vip/wangyoudongman'],
+    ['军事历史','https://www.zei8.vip/junshilishi'],
+    ['热点资源','https://www.zei8.vip/redianziyuan'],
+    ['西方奇幻','https://www.zei8.vip/xifangqihuan'],
+    ['仙侠修真','https://www.zei8.vip/xianxiaxiuzhen'],
+    ['侦探推理','https://www.zei8.vip/zhentantuili'],
+    ['经典科幻','https://www.zei8.vip/jingdiankehuan'],
+    ['名著杂志','https://www.zei8.vip/mingzhuzazhi'],
+    ['历史穿越','https://www.zei8.vip/lishichuanyue'],
+    ['传统言情','https://www.zei8.vip/chuantongyanqing'],
+    ['都市社会','https://www.zei8.vip/dushishehui'],
+    ['幻想现言', 'https://www.zei8.vip/huanxiangxianyan'],
+    ['耽美百合', 'https://www.zei8.vip/danmeibaihe']
 ]
 # https://www.zei8.vip/jingdiankehuan/index_2.html
 
-classUrl = classList[0]
+# classUrl = classList[0]
 _temp_img = r'D:/_temp_screenshot.png'
+recordPath = r'C:\Users\Administrator\Desktop\zei8Novel'
+
 
 async def main():
 
@@ -41,10 +44,30 @@ async def main():
     # await page.waitForSelector('div.listbox')
     # await page.screenshot({'path': _temp_img})
 
-    # 最大页数
-    await page.waitForSelector('ul.pagelist > li:last-child > a')
-    maxCount = await (await (await page.J('ul.pagelist > li:last-child > a')).getProperty('href')).jsonValue()
-    print(maxCount)
+    for className, classUrl in classList:
+        # 最大页数
+        await page.goto(classUrl)
+        await page.waitForSelector('ul.pagelist > li:last-child > a')
+        maxCount = int((re.match(r'.*index\_(\d+).html', await (await (await page.J('ul.pagelist > li:last-child > a')).getProperty('href')).jsonValue()).group(1)))
+        # maxCount = re.match(r'index\_(\d+).html', maxCount).group(1)
+        # print(maxCount)
+        # 收集子页面
+        print('访问Class:{}.首页,共{}页'.format(className, maxCount))
+        pagesData = [[await(await item.getProperty('textContent')).jsonValue(), await(await item.getProperty('href')).jsonValue()] for item in await page.JJ('div.listbox > ul > li > dl > a')]
+        for index in range(2, maxCount+1):
+            print('访问Class:{}.{}'.format(className, index))
+            await page.goto('https://www.zei8.vip/xuanhuan/index_{}.html'.format(index))
+            await page.waitForSelector('ul.pagelist > li:last-child > a')
+            pagesData += [[await(await item.getProperty('textContent')).jsonValue(), await(await item.getProperty('href')).jsonValue()] for item in await page.JJ('div.listbox > ul > li > dl > a')]
+        # 记录子页面
+        recordFile = r'{}\{}-{}.ini'.format(recordPath,className, len(pagesData))
+        print('获取完成,记录到文件:{}'.format(recordFile))
+        if os.path.isfile(recordFile):
+            os.remove(recordFile)
+        with open(recordFile, 'a', encoding='utf-8') as rf:
+            for pageName, pageUrl in pagesData:
+                rf.write('{}\n{}\n'.format(pageName, pageUrl))
 
+    await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())

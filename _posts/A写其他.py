9f67c33dcb7a17a04842de270dae0c80926b0ff2
a690089735 <690089735@qq.com>
2023-02-13 14:33:56 +0800

    tt

---
+++ _posts/A写其他.py
@@ -0,0 +1,10 @@
+import time
+import os
+p = os.path.dirname(os.path.abspath(__file__))
+type = '其他'
+title = input('文章标题:')
+fileName = '{}\\{}-{}-{}.md'.format(p, time.strftime(
+    '%Y-%m-%d', time.localtime()), type, title)
+with open(fileName, 'w', encoding="utf-8") as f:
+    f.write('---\nlayout: post\ntitle: {}\ndate: {}\ncategories: {}\ntags: {}\ncomments: false\ntypora-root-url: ..\n---\n\n© {} 无名玩家X'.format(title, time.strftime("%Y-%m-%d %H:%M:%S.00 +08:00", time.localtime()), type, type, time.strftime('%Y', time.localtime())))
+os.system('start \"\" \"{}\"'.format(fileName))
--- _posts/A写笔记.py
+++ _posts/A写笔记.py
@@ -1,6 +1,10 @@
-import time,os
+import time
+import os
 p = os.path.dirname(os.path.abspath(__file__))
-fn = '{}\\{}-笔记-博客文章.md'.format(p,time.strftime('%Y-%m-%d', time.localtime()))
-with open(fn,'w',encoding="utf-8") as f:
-    f.write('---\nlayout: post\ntitle: 博客标题\ndate: {}\ncategories: 笔记\ntags: 笔记\ncomments: false\ntypora-root-url: ..\n---\n\n文章内容'.format(time.strftime("%Y-%m-%d %H:%M:%S.00 +08:00", time.localtime())))
-os.system('start \"\" \"{}\"'.format(fn))
+type = '笔记'
+title = input('文章标题:')
+fileName = '{}\\{}-{}-{}.md'.format(p, time.strftime(
+    '%Y-%m-%d', time.localtime()), type, title)
+with open(fileName, 'w', encoding="utf-8") as f:
+    f.write('---\nlayout: post\ntitle: {}\ndate: {}\ncategories: {}\ntags: {}\ncomments: false\ntypora-root-url: ..\n---\n\n© {} 无名玩家X'.format(title, time.strftime("%Y-%m-%d %H:%M:%S.00 +08:00", time.localtime()), type, type, time.strftime('%Y', time.localtime())))
+os.system('start \"\" \"{}\"'.format(fileName))

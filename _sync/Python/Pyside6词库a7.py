a060a053df8ff073e8fa7b660aa432c7367b6c98
a690089735 <690089735@qq.com>
2023-03-09 20:02:22 +0800

    t'ttt

--- _sync/Python/Pyside6词库a7.py
+++ _sync/Python/Pyside6词库a7.py
@@ -22,11 +22,30 @@ import random
 import json
 import os
 
+
+'''
+质量前缀+前置画风引导+前置镜头效果+前置光照效果+[带描述的物X+物X的各种次要物+镜头效果和光照]*X+全局光照效果+全局镜头效果+画风滤镜 作者：日记声音 https://www.bilibili.com/read/cv19552237?from=search 出处：bilibili
+质量指示,人物元素,服装饰品,场景元素,画面特效,画风滤镜 -法典
+1.Lexica.art
+2.KREA.ai
+3.promptoMANIA
+4.OpenArt.ai
+质量指示,画风引导,镜头效果,光照效果,人物元素,服装饰品,场景元素,纹理细节,画面特效,画风滤镜
+(强调:1.1)[弱化:1.1]
+'''
+
 dataPath = os.path.join(os.path.dirname(
     sys.argv[0]), r'StableDiffusionTagsLib.json')
 with open(dataPath, 'r', encoding='utf-8') as f:
     jsonData: dict = json.load(f)
 
+def abstractText(t: str) -> str:
+    if len(t) > 28:
+        return t[:25]+'...'
+    else:
+        return t
+# abstractText('dasddddqwdewd,sddsd34dafdgwrrfsfa,asafafaffd')
+# abstractText('dasddddqwdewd,sd')
 
 # 更改布局,让滚动在tabs之下
 class WordLibrary(QTabWidget):
@@ -51,7 +70,7 @@ class WordLibrary(QTabWidget):
                         f'{parameter}', group)
                     qPushButton.setCheckable(True)
                     qPushButton.clicked.connect(
-                        (lambda qPushButton=qPushButton, text=value['text'], order=value['order']: self.buttonClicked(qPushButton, text, order))) # 这里坑了很长时间,lambda表达式在循环中有惰性问题(只在运行时找变量),所以指定一下lambda表达式的默认参数
+                        (lambda qPushButton=qPushButton, text=value['text'], order=value['order']: self.buttonClicked(qPushButton, text, order)))  # 这里坑了很长时间,lambda表达式在循环中有惰性问题(只在运行时找变量),所以指定一下lambda表达式的默认参数
                     layout.addWidget(qPushButton)
 
                 container_layout.addWidget(group)
@@ -102,7 +121,7 @@ if __name__ == '__main__':
             border_layout.addWidget(controlBoard, Position.East)
             self.setLayout(border_layout)
             self.setMinimumWidth(480)
-            self.setWindowTitle('StableDiffusion词库')
+            self.setWindowTitle('AI绘画辞典')
 
     app = QApplication(sys.argv)
     window = Window()

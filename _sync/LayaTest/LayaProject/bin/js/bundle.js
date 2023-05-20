5140f5f4a056b28143b371733d9db1b44899873d
a690089735 <690089735@qq.com>
2023-05-20 13:26:24 +0800

    tt

---
+++ _sync/LayaTest/LayaProject/bin/js/bundle.js
@@ -0,0 +1,94 @@
+(() => {
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+  var __decorateClass = (decorators, target, key, kind) => {
+    var result = kind > 1 ? void 0 : kind ? __getOwnPropDesc(target, key) : target;
+    for (var i = decorators.length - 1, decorator; i >= 0; i--)
+      if (decorator = decorators[i])
+        result = (kind ? decorator(target, key, result) : decorator(result)) || result;
+    if (kind && result)
+      __defProp(target, key, result);
+    return result;
+  };
+
+  // src/DragMove.ts
+  var { regClass, property } = Laya;
+  var Sprite = Laya.Sprite;
+  var DragMove = class extends Laya.Script {
+    get sprite() {
+      return this._sprite;
+    }
+    set sprite(value) {
+      this._sprite = value;
+    }
+    // constructor() {
+    //     super();
+    // }
+    /**
+     * 组件被激活后执行，此时所有节点和组件均已创建完毕，此方法只执行一次
+     */
+    //onAwake(): void {}
+    /**
+     * 组件被启用后执行，例如节点被添加到舞台后
+     */
+    //onEnable(): void {}
+    /**
+     * 组件被禁用时执行，例如从节点从舞台移除后
+     */
+    //onDisable(): void {}
+    /**
+     * 第一次执行update之前执行，只会执行一次
+     */
+    //onStart(): void {}
+    /**
+     * 手动调用节点销毁时执行
+     */
+    //onDestroy(): void {
+    /**
+     * 每帧更新时执行，尽量不要在这里写大循环逻辑或者使用getComponent方法
+     */
+    //onUpdate(): void {}
+    /**
+     * 每帧更新时执行，在update之后执行，尽量不要在这里写大循环逻辑或者使用getComponent方法
+     */
+    //onLateUpdate(): void {}
+    /**
+     * 鼠标点击后执行。与交互相关的还有onMouseDown等十多个函数，具体请参阅文档。
+     */
+    //onMouseClick(): void {}
+    onMouseDown(evt) {
+      this._offsetX = this._sprite.x - evt.stageX;
+      this._offsetY = this._sprite.y - evt.stageY;
+    }
+    onMouseDrag(evt) {
+      this._sprite.x = evt.stageX + this._offsetX;
+      this._sprite.y = evt.stageY + this._offsetY;
+    }
+    onMouseDragEnd(evt) {
+    }
+    // onMouseUp(evt: Laya.Event): void {
+    //     console.log("抬起")
+    // }
+  };
+  __name(DragMove, "DragMove");
+  __decorateClass([
+    property({ type: Sprite })
+  ], DragMove.prototype, "sprite", 1);
+  DragMove = __decorateClass([
+    regClass("ad0e797a-dc3d-491a-bba5-ab456c661981", "../src/DragMove.ts")
+  ], DragMove);
+
+  // src/Main.ts
+  var { regClass: regClass2, property: property2 } = Laya;
+  var Main = class extends Laya.Script {
+    onStart() {
+      console.log("Game start");
+    }
+  };
+  __name(Main, "Main");
+  Main = __decorateClass([
+    regClass2("7bad1742-6eed-4d8d-81c0-501dc5bf03d6", "../src/Main.ts")
+  ], Main);
+})();
+//# sourceMappingURL=bundle.js.map
---
+++ _sync/LayaTest/LayaProject1/bin/js/bundle.js
@@ -0,0 +1,27 @@
+(() => {
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+  var __decorateClass = (decorators, target, key, kind) => {
+    var result = kind > 1 ? void 0 : kind ? __getOwnPropDesc(target, key) : target;
+    for (var i = decorators.length - 1, decorator; i >= 0; i--)
+      if (decorator = decorators[i])
+        result = (kind ? decorator(target, key, result) : decorator(result)) || result;
+    if (kind && result)
+      __defProp(target, key, result);
+    return result;
+  };
+
+  // src/Main.ts
+  var { regClass, property } = Laya;
+  var Main = class extends Laya.Script {
+    onStart() {
+      console.log("Game start");
+    }
+  };
+  __name(Main, "Main");
+  Main = __decorateClass([
+    regClass("7bad1742-6eed-4d8d-81c0-501dc5bf03d6", "../src/Main.ts")
+  ], Main);
+})();
+//# sourceMappingURL=bundle.js.map

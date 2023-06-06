dafbb14452cd381c167b039415bc670f891acaec
a690089735 <690089735@qq.com>
2023-06-06 19:28:30 +0800

    Update Vector2D.ts

--- _sync/LayaTest/LayaProject/src/Vector2D数学/Vector2D.ts
+++ _sync/LayaTest/LayaProject/src/Vector2D数学/Vector2D.ts
@@ -23,18 +23,18 @@ export class Vector2D {
   y: number;
 
   /**
-   * 创建一个空 <code>Vector2</code> 实例。
+   * 创建一个 <code>Vector2D</code> 实例。
    * @param	x = 0  X轴坐标。
    * @param	y = 0  Y轴坐标。
    */
   constructor(); // 无参数构建0向量
   /**
-   * 从角度创建一个 <code>Vector2</code> 实例。
+   * 从角度创建一个 <code>Vector2D</code> 实例。
    * @param	angle  旋转角度。
    */
   constructor(angle: number); // 从角度构建向量
   /**
-   * 创建一个 <code>Vector2</code> 实例。
+   * 创建一个 <code>Vector2D</code> 实例。
    * @param	x  X轴坐标。
    * @param	y  Y轴坐标。
    */
@@ -56,48 +56,86 @@ export class Vector2D {
   }
 
   //实例版本
+  /**
+   * 2D向量加法
+   * @param	b  另一个向量.
+   * @return 新的2D向量
+   */
   public add(b: Vector2D): Vector2D {
     return new Vector2D(this.x + b.x, this.y + b.y);
   }
-
+  /**
+   * 2D向量减法
+   * @param	b  另一个向量.
+   * @return 新的2D向量
+   */
   public sub(b: Vector2D): Vector2D {
     return new Vector2D(this.x - b.x, this.y - b.y);
   }
-
+  /**
+   * 2D向量乘法
+   * @param	b  另一个向量.
+   * @return 新的2D向量
+   */
   public mul(b: Vector2D): Vector2D {
     return new Vector2D(this.x * b.x, this.y * b.y);
   }
-
+  /**
+   * 2D向量除法
+   * @param	b  另一个向量.
+   * @return 新的2D向量
+   */
   public div(b: Vector2D): Vector2D {
     return new Vector2D(this.x / b.x, this.y / b.y);
   }
-
+  /**
+   * 2D向量缩放(标量乘法)
+   * @param	b  另一个向量.
+   * @return 新的2D向量
+   */
   public scale(b: number): Vector2D {
     return new Vector2D(this.x * b, this.y * b);
   }
-
+  /**
+   * 2D向量点乘
+   * @param	b  另一个向量.
+   * @return 两个向量的点乘值(未规一化)
+   */
   public dot(b: Vector2D): number {
     return this.x * b.x + this.y * b.y;
   }
-
+  /**
+   * 2D向量求距离
+   * @param	b  另一个向量.
+   * @return 两个向量之间的距离
+   */
   public distance(b: Vector2D): number {
     const dx = this.x - b.x;
     const dy = this.y - b.y;
     return Math.sqrt(dx * dx + dy * dy);
   }
-
+  /**
+   * 2D向量减法
+   * @return 向量的模长 
+   */
   public magnitude(): number {
     return Math.sqrt(this.x * this.x + this.y * this.y);
   }
-
+  /**
+   * 2D向量归一化
+   * @return 新的2D向量
+   */
   public normalize(): Vector2D {
     const len = this.magnitude();
     if (len === 0) {
-      return new Vector2D(0, 0);
+      return new Vector2D(this.x, this.y);
     }
     return new Vector2D(this.x / len, this.y / len);
   }
-
+  /**
+   * 2D向量求角度
+   * @return 向量角度
+   */
   public toAngle(): number {
     var angle = Math.atan2(this.y, this.x) * 180 / Math.PI;
     if (angle < 0) {
@@ -105,13 +143,27 @@ export class Vector2D {
     }
     return angle;
   }
-
-  // 向量旋转一定角度
+  /**
+   * 2D向量旋转
+   * @param	angle  旋转角度
+   * @return 新的2D向量
+   */
   public rotate(angle: number): Vector2D {
     const radian = angle * Math.PI / 180;
     const cos = Math.cos(radian);
     const sin = Math.sin(radian);
     return new Vector2D(this.x * cos - this.y * sin, this.x * sin + this.y * cos);
+  }
+
+  /**
+   * 求两个2D向量夹角
+   * @param	b  另一个向量.
+   * @return 两个向量之间的角度
+   */
+  public angleTo(b: Vector2D): number {
+    const dot = this.x * b.x + this.y * b.y;
+    const det = this.x * b.y - this.y * b.x;
+    return Math.atan2(det, dot) * 180 / Math.PI;
   }
 
 

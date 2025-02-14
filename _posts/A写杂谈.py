8931027a9424b37501266dd5adb2cbb820ee910e
a690089735 <690089735@qq.com>
2025-02-14 13:29:55 +0800

    tt

--- _posts/A写杂谈.py
+++ _posts/A写杂谈.py
@@ -8,3 +8,7 @@ fileName = '{}\\{}-{}-{}.md'.format(p, time.strftime(
 with open(fileName, 'w', encoding="utf-8") as f:
     f.write('---\nlayout: post\ntitle: {}\ndate: {}\ncategories: {}\ntags: {}\ncomments: false\ntypora-root-url: ..\n---\n\n© {} 无名玩家X'.format(title, time.strftime("%Y-%m-%d %H:%M:%S.00 +08:00", time.localtime()), type, type, time.strftime('%Y', time.localtime())))
 os.system('start \"\" \"{}\"'.format(fileName))
+
+
+
+
